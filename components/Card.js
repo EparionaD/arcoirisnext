71ffade1a1058591e54db7d69dcaa7597e5beda9
Ernesto Pariona Díaz <eparionad@gmail.com>
2023-06-06 22:57:56 -0500

    update projects+

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,30 @@
 import styles from '../styles/Card.module.css';
 import style1 from '../styles/Produccion.module.css';
+import data from '../utils/projects.json';
+
 import Modal from './Modal';
 import { useState } from 'react';
 
+const getUser = (slug) => {
+  const details = [];
+  data.data.map((project) => {
+    project.slug == slug && details.push(project.complete);
+  });
+
+  return details;
+};
+
 const Card = ({ projects }) => {
   const [modal, setModal] = useState(false);
+  const [detailComplete, setDetailComplete] = useState();
+  // console.log(detailComplete);
+
+  const projectClick = (slug1) => {
+    const pro = getUser(slug1);
+    setDetailComplete(pro);
+    setModal(!modal);
+  };
+
   return (
     <section className={style1.produccion}>
       {projects.map((project) => (
@@ -17,16 +37,33 @@ const Card = ({ projects }) => {
         >
           <div
             className={styles.card__content}
-            onClick={() => setModal(!modal)}
+            onClick={() => projectClick(project.slug)}
           >
             <h3 className={styles.card__title}>{project.title}</h3>
             <p className={styles.card__text}>{project.summary}</p>
           </div>
-          <Modal
+          {project.slug == 'danza' && (
+            <Modal
+              estado={modal}
+              cambiarEstado={setModal}
+              complete={detailComplete}
+              initial={project.video1}
+            />
+          )}
+          {project.slug == 'contratado' && (
+            <Modal
+              estado={modal}
+              cambiarEstado={setModal}
+              complete={detailComplete}
+              initial={project.video1}
+            />
+          )}
+          {/* <Modal
             estado={modal}
             cambiarEstado={setModal}
-            complete={project.complete}
-          />
+            complete={detailComplete}
+            initial={project.video1}
+          /> */}
         </div>
       ))}
     </section>
--- components/Modal.js
+++ components/Modal.js
@@ -3,8 +3,14 @@ import { IoMdClose } from 'react-icons/io';
 import Image from 'next/image';
 import { useState } from 'react';
 
-const Modal = ({ estado, cambiarEstado, complete }) => {
-  const [imagen, setImagen] = useState(complete.video);
+const Modal = ({ estado, cambiarEstado, complete, initial }) => {
+  const [imagen, setImagen] = useState('');
+
+  const closeModal = () => {
+    setImagen('');
+    cambiarEstado(false);
+  };
+
   return (
     <>
       {estado && (
@@ -13,82 +19,87 @@ const Modal = ({ estado, cambiarEstado, complete }) => {
             <div className={styles.modal__close}>
               <IoMdClose
                 className={styles.modal__icon}
-                onClick={() => cambiarEstado(false)}
+                onClick={() => closeModal()}
               />
             </div>
-            <div className={styles.modal__container}>
-              <div>
-                <div>
-                  {imagen.startsWith('http') ? (
-                    <iframe
-                      width='100%'
-                      height='500'
-                      src={`${imagen}`}
-                      title='YouTube video player'
-                      frameborder='0'
-                      allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-                      allowfullscreen
-                    ></iframe>
-                  ) : (
-                    <Image
-                      layout='responsive'
-                      width={900}
-                      height={700}
-                      src={imagen}
-                    />
-                  )}
+
+            <div>
+              {complete.map((comple) => (
+                <div className={styles.modal__container} key={comple.title}>
+                  <div>
+                    <div>
+                      {imagen.startsWith('https://www') ? (
+                        <iframe
+                          width='100%'
+                          height='500'
+                          src={`${imagen}`}
+                          title='YouTube video player'
+                          frameborder='0'
+                          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                          allowfullscreen
+                        ></iframe>
+                      ) : (
+                        <Image
+                          layout='responsive'
+                          width={900}
+                          height={500}
+                          src={imagen}
+                        />
+                      )}
+                    </div>
+                    <div className={styles.thumbnail}>
+                      <Image
+                        layout='responsive'
+                        width={900}
+                        height={700}
+                        src={comple.videoImagen}
+                        className={styles.modal__image}
+                        onClick={() => setImagen(comple.video)}
+                      />
+                      <Image
+                        layout='responsive'
+                        width={900}
+                        height={700}
+                        src={comple.imagen}
+                        className={styles.modal__image}
+                        onClick={() => setImagen(comple.imagen)}
+                      />
+                      <Image
+                        layout='responsive'
+                        width={900}
+                        height={700}
+                        src={comple.imagen2}
+                        className={styles.modal__image}
+                        onClick={() => setImagen(comple.imagen2)}
+                      />
+                      <Image
+                        layout='responsive'
+                        width={900}
+                        height={700}
+                        src={comple.imagen3}
+                        className={styles.modal__image}
+                        onClick={() => setImagen(comple.imagen3)}
+                      />
+                      <Image
+                        layout='responsive'
+                        width={900}
+                        height={700}
+                        src={comple.imagen4}
+                        className={styles.modal__image}
+                        onClick={() => setImagen(comple.imagen4)}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.modal__text}>
+                    <h3 className={styles.modal__title}>{comple.title}</h3>
+                    <div>
+                      <p className={styles.modal__description}>
+                        {comple.description}
+                      </p>
+                    </div>
+                  </div>
                 </div>
-                <div className={styles.thumbnail}>
-                  <Image
-                    layout='responsive'
-                    width={900}
-                    height={700}
-                    src={complete.videoImagen}
-                    className={styles.modal__image}
-                    onClick={() => setImagen(complete.video)}
-                  />
-                  <Image
-                    layout='responsive'
-                    width={900}
-                    height={700}
-                    src={complete.imagen}
-                    className={styles.modal__image}
-                    onClick={() => setImagen(complete.imagen)}
-                  />
-                  <Image
-                    layout='responsive'
-                    width={900}
-                    height={700}
-                    src={complete.imagen2}
-                    className={styles.modal__image}
-                    onClick={() => setImagen(complete.imagen2)}
-                  />
-                  <Image
-                    layout='responsive'
-                    width={900}
-                    height={700}
-                    src={complete.imagen3}
-                    className={styles.modal__image}
-                    onClick={() => setImagen(complete.imagen3)}
-                  />
-                  <Image
-                    layout='responsive'
-                    width={900}
-                    height={700}
-                    src={complete.imagen4}
-                    className={styles.modal__image}
-                    onClick={() => setImagen(complete.imagen4)}
-                  />
-                </div>
-              </div>
-              <div className={styles.modal__text}>
-                <h3 className={styles.modal__title}>{complete.title}</h3>
-                <div>
-                  <p className={styles.modal__description}>
-                    {complete.description}
-                  </p>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
